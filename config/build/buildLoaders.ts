5a1a0cbc6495c9ade2b60b706a616bfcb1d95dcd
Sergei Vedeneev <served9@yandex.ru>
2022-09-19 19:21:22 +0300

    Урок 4-7

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,29 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { RuleSetRule } from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev
+        ? 'style-loader'
+        : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path].[name]__[local]'
+              : '[hash:base64:8]'
+          }
+        },
+      },
+      "sass-loader",
+    ],
+  }
 
   const typescriptLoader =
   {
@@ -10,6 +33,7 @@ export function buildLoaders(): RuleSetRule[] {
   }
 
   return [
-    typescriptLoader
+    typescriptLoader,
+    cssLoader
   ]
 }
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import './index.scss'
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О приложении</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
