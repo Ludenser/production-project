67acf455f1c06a58c06b0a7b712a24bec8a0de0e
Sergei Vedeneev <served9@yandex.ru>
2022-09-20 21:08:17 +0300

    Урок 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
+
+  const { theme, toggleTheme } = useTheme()
   return (
-    <div className="app">
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>Сменить тему</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О приложении</Link>
       <Suspense fallback={<div>Loading...</div>}>
