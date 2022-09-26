9f134b1c7b0571e04d48dc002d2ddf5824a1bacb
Sergei Vedeneev <served9@yandex.ru>
2022-09-26 03:44:33 +0300

    19 урок stylelint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,22 +1,25 @@
+import { RoutePath } from 'app/providers/router/routeConfig/RouteConfig';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
-interface NavbarProps {
-  className?: string;
-}
-
-export const Navbar = ({ className }: NavbarProps) => {
+export const Navbar = () => {
     const { t } = useTranslation();
     return (
         <div className={classNames(cls.Navbar, {}, [])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.main}
+                    className={cls.mainLink}
+                >
                     {t('Main-page-link')}
                 </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.about}
+                >
                     {t('About-page-link')}
                 </AppLink>
             </div>
