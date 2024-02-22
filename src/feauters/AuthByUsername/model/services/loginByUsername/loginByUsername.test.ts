797dacfb8a9a0b3cc93e39becff98d9ddc3d617f
Ludenser <served9@yandex.ru>
2024-02-22 15:42:19 +0400

    fix test

--- src/feauters/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/feauters/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,11 +1,7 @@
-import axios from 'axios';
 import { userActions } from 'entities/User';
 import { TestAsynkThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './loginByUsername';
 
-jest.mock('axios');
-
-const mockedAxios = jest.mocked(axios, true);
 describe('loginByUsername.test', () => {
     // let dispatch: Dispatch;
     // let getState: ()=>StateSchema;
@@ -40,25 +36,23 @@ describe('loginByUsername.test', () => {
 
     test('success login', async () => {
         const userValue = { username: '123', id: '1' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
 
         const thunk = new TestAsynkThunk(loginByUsername);
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
         expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(result.payload).toBe(userValue);
     });
 
     test('error login', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
-
         const thunk = new TestAsynkThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(thunk.dispatch).toHaveBeenCalledTimes(2);
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toBe('Incorrect login or password');
